d6f51b86792dfbbf23485fd0532611e540bf94ef
Rik Meijer <rik@rikmeijer.nl>
2021-12-14 16:26:45 +0100

    import normalize css in app.css

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,7 +14,4 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
-    ])
-    .postCss('resources/css/normalize.css', 'public/css', [
-        //
     ]);
